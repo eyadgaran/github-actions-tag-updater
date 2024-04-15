da260145941c845a24279630da5a0212171b66e9
Elisha Yadgaran <elishay@alum.mit.edu>
2024-04-14 19:35:52 -0700

    reformat parameters

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 Push new tags automatically via Github Actions
 
-Configurable to increment last tag (globally or per branch) or apply a fixed tag.
+Configurable to increment last tag with custom prefix, message, and increment amount.
 
 ## Input parameters
 
 These are the parameters you can use with the action:
 
-- `increment_branch_tag`: [optional] Whether tag versions should be relative to the current branch or globally in the repo (e.g. latest tag on branch is 2.1.0 and 4.0.0 on another branch)
 - `message`: [optional] Message for the tag
-- `tag_prefix`: [optional] String to be added before the tag, for example if this parameter takes the value "v" the final tag will be "v8.0.0"
+- `prefix`: [optional] String to be added before the tag, for example if this parameter takes the value "v" the final tag will be "v8.0.0" (can pass branch name as prefix to make versioning per branch)
+- `version`: [optional] Version field to be used as the base for the new tag, if not provided the patch version will be incremented. Allowed values are "major", "minor", "patch"
+- `increment`: [optional] Increment amount for the version field, default is 1
 
 ## Usage
 
@@ -24,19 +25,19 @@ on:
     branches: 'master'
 
 jobs:
-  build:
-    name: Bump tag
+  tag:
+    permissions: write-all
+    name: Bump tag version
     runs-on: ubuntu-latest
-
     steps:
-    - uses: actions/checkout@v2
+    - uses: actions/checkout@v4
 
     - name: Create an incremental release
       uses: eyadgaran/github-actions-tag-updater@master
       with:
-        increment_branch_tag: true
+        version: 'patch'
         message: Bump version
-        tag_prefix: 'v'
+        prefix: 'v'
       env:
         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,11 @@ git_setup ( ) {
 echo "###################"
 echo "Tagging Parameters"
 echo "###################"
-echo "Version by Branch: ${INPUT_INCREMENT_BRANCH_TAG}"
-echo "Version Increment: ${VERSION_INCREMENT}"
+echo "Version to Increment: ${INPUT_VERSION}"
+echo "Increment Amount: ${INPUT_INCREMENT}"
 echo "Tag Message: ${INPUT_MESSAGE}"
-echo "Tag Prefix: ${INPUT_TAG_PREFIX}"
+echo "Tag Prefix: ${INPUT_PREFIX}"
+echo "Tag Prefix: ${INPUT_PREFIX}"
 echo "GITHUB_ACTOR: ${GITHUB_ACTOR}"
 echo "GITHUB_TOKEN: ${GITHUB_TOKEN}"
 echo "HOME: ${HOME}"
@@ -27,26 +28,14 @@ git_setup
 echo "2) Updating repository tags..."
 git fetch origin --tags --quiet
 
-last_tag=""
-if [ "${INPUT_INCREMENT_BRANCH_TAG}" = true ];then
-    branch=$(git rev-parse --abbrev-ref HEAD)
-    echo "Branch: ${branch}";
+# Get last tag
+last_tag=`git describe --tags $(git rev-list --tags) --always|egrep "${INPUT_PREFIX}\.[0-9]*\.[0-9]*\.[0-9]*$"|sort -V -r|head -n 1`
+echo "Last Tag: ${last_tag}";
 
-    last_tag=`git describe --tags $(git rev-list --tags) --always|egrep "${branch}-${INPUT_TAG_PREFIX}\.[0-9]*\.[0-9]*\.[0-9]*$"|sort -V -r|head -n 1`
-    echo "Last Branch Tag: ${last_tag}";
-else
-    last_tag=`git describe --tags $(git rev-list --tags --max-count=1)`
-    echo "Last Tag: ${last_tag}";
-fi
-
-
+# Default first tag
 if [ -z "${last_tag}" ];then
-    if [ "${INPUT_INCREMENT_BRANCH_TAG}" != false ];then
-        last_tag="${branch}-${INPUT_TAG_PREFIX}0.0.0";
-    else
-        last_tag="${INPUT_TAG_PREFIX}0.0.0";
-    fi
-    echo "Default Last tag: ${last_tag}";
+    last_tag="${INPUT_PREFIX}0.0.0";
+    echo "Default First tag: ${last_tag}";
 fi
 
 # Extract major, minor, and patch versions from the last tag
@@ -54,30 +43,26 @@ major_version=$(echo "${last_tag}" | awk -F'.' '{print $1}')
 minor_version=$(echo "${last_tag}" | awk -F'.' '{print $2}')
 patch_version=$(echo "${last_tag}" | awk -F'.' '{print $3}')
 
-# Increment the version based on the value of VERSION_INCREMENT
-if [ "${VERSION_INCREMENT}" = "major" ]; then
-    next_major_version=$((major_version + 1))
+# Increment the version based on the value of INPUT_VERSION
+if [ "${INPUT_VERSION}" = "major" ]; then
+    next_major_version=$((major_version + ${INPUT_INCREMENT}))
     next_minor_version=0
     next_patch_version=0
-elif [ "${VERSION_INCREMENT}" = "minor" ]; then
+elif [ "${INPUT_VERSION}" = "minor" ]; then
     next_major_version=${major_version}
-    next_minor_version=$((minor_version + 1))
+    next_minor_version=$((minor_version + ${INPUT_INCREMENT}))
     next_patch_version=0
-elif [ "${VERSION_INCREMENT}" = "patch" ]; then
+elif [ "${INPUT_VERSION}" = "patch" ]; then
     next_major_version=${major_version}
     next_minor_version=${minor_version}
-    next_patch_version=$((patch_version + 1))
+    next_patch_version=$((patch_version + ${INPUT_INCREMENT}))
 else
-    echo "Invalid value for VERSION_INCREMENT: ${VERSION_INCREMENT}"
+    echo "Invalid value for INPUT_VERSION: ${INPUT_VERSION}"
     exit 1
 fi
 
 # Construct the next tag
-if [ "${INPUT_INCREMENT_BRANCH_TAG}" = true ];then
-    next_tag="${branch}-${INPUT_TAG_PREFIX}${next_major_version}.${next_minor_version}.${next_patch_version}"
-else
-    next_tag="${INPUT_TAG_PREFIX}${next_major_version}.${next_minor_version}.${next_patch_version}"
-fi
+next_tag="${INPUT_TAG_PREFIX}${next_major_version}.${next_minor_version}.${next_patch_version}"
 
 echo "3) Next Tag: ${next_tag}";
 
